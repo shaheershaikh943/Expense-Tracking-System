1a8d0da013001f6f778ae62723c58752c56e78f2
Shaheer <shaheershaikh943@gmail.com>
2026-05-12 04:17:28 -0700

    Moved to csv format and deleted the Expenses.txt

--- Personal Expense Tracker System.py
+++ Personal Expense Tracker System.py
@@ -5,39 +5,47 @@ def Add_Expenses():
      expense_category = input("Enter the Category of Expense:")
      expense_amount = float(input("Enter the amount of Expense:"))
      expense_data = View_Expenses()
-     id = f"Expense-{len(expense_data)+1}"
-     with open(file="Expenses.txt",mode="a") as file:
-          data = f"{id},{expense_date},{expense_category},{expense_amount}\n"
+     expense_id = f"Expense-{len(expense_data)+1}"
+     with open(file="Expenses.csv",mode="a") as file:
+          data = f"{expense_id},{expense_date},{expense_category},{expense_amount}\n"
           file.write(data)
+          print("Expense Added Successfully!!!")
 
 def View_Expenses():
     try:
         with open(file="Expenses.txt",mode="r") as file:
             expense_data = [line.strip().split(",") for line in file.readlines()]
             return expense_data
-    except FileNotFoundError:
+    except FileNotFoundError as e:
+        print(f"{e}")
         return []
     except Exception as e:
+        print(f"{e}")
         return []
 
 def Total_Expense():
     expense_data = View_Expenses()
     total_expenses = 0
-    for i in range(len(expense_data)):
-        amount = expense_data [i][3]
-        total_expenses += amount
-        print(f"Total Expense : {total_expenses}")
+    for expense in expense_data:
+            amount = float(expense [3])
+            total_expenses += amount
+    print(f"Total Expense : {total_expenses}")
 
-def Search_By_id(id):
+def Search_By_id(expense_id):
     expense_data = View_Expenses()
+    Found = False
     for line in expense_data:
-        if line [0] == id:
-            print(f"id : {line[0]}")
-            print(f"Date : {line[1]}")
-            print(f"Category : {line[2]}")
-            print(f"amount : {line[3]}")
-    else:
-        print("id is not Registered!!!")
+        if line [0] == expense_id:
+            Found = True
+            print(f"""
+                        Expense Id : {line[0]}
+                        Date : {line[1]}
+                        Category : {line[2]}
+                        amount : {line[3]}
+                    """)
+                
+    if not Found:
+        print("Expense Id is not Registered!!!")
 
 # <---------Functions--------->
 
@@ -46,7 +54,7 @@ def Search_By_id(id):
 while True:
     while True:
         try:
-            menu_choice = int(input("1.Add Expense\n2.View Expenses\n3.Total Expense\n4.Search By id\n5.Exit\nEnter Your Choice:"))
+            menu_choice = int(input("1.Add Expense\n2.View Expenses\n3.Total Expense\n4.Search By expense_id\n5.Exit\nEnter Your Choice:"))
             break
         except Exception as e:
             print(f"Error:{e}")
@@ -54,11 +62,12 @@ while True:
         Add_Expenses()
     elif menu_choice == 2:
         view_expense = View_Expenses()
-        print(f"View_Expense")
+        for expense in view_expense:
+            print(f"{expense}")
     elif menu_choice == 3:
         Total_Expense()
     elif menu_choice == 4:
-        Search_By_id(id = input("Enter the id of Expense:"))
+        Search_By_id(expense_id = input("Enter the Expense Id of Expense:"))
     elif menu_choice == 5:
         print("Exiting Program...")
         exit()
